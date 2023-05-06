57fea03c1850d2363c596268be80dfefd9a3bac9
Suganth <97389489+Suganth056@users.noreply.github.com>
2023-05-06 21:15:32 +0530

    Add files via upload

---
+++ publicAccess.java
@@ -0,0 +1,10 @@
+package accessSpecifier;
+public class publicAccess {
+	public int c=66;
+	public static void main(String[] args) {
+		//publicAccess obj1=new publicAccess();
+		 int a=4;
+		 System.out.println(a);
+	}
+
+}
---
+++ publicAccess2.java
@@ -0,0 +1,15 @@
+package accessSpecifier;
+import accessSpecifier5.*;
+public class publicAccess2 {
+		public static void main(String[] args) {
+			//publicAccess obj=new publicAccess();
+			//Creating object for another package......
+			//AccessSpecify rn=new AccessSpecify();
+			//System.out.println(rn.t);//protected private and default can't be accessible
+			 //System.out.println(obj2.u);
+			//System.out.println(obj.c);
+			//int b=6;
+			int number=10;
+			System.out.println(++number + number++);
+		}
+}
